46181c3892350f596518a385931c247b7cc10232
Otavio Fontes <otcfontes@gmail.com>
2022-09-04 20:57:37 -0300

    modificados

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,57 @@
 console.log('Hello World!');
 console.log('Olá Mundo!');
 console.log('Git Pull!');
+
+//next
+
+let minhaVariavel;
+let outraVariavel = 1;
+minhaVariavel = 'Ituring';
+
+console.log(minhaVariavel);
+console.log(outraVariavel);
+
+let variavel1 = 'Texto aqui';
+console.log(variavel1);
+
+let variavel2 = 1;
+console.log(variavel1);
+console.log(variavel2);
+
+variavel1 = 'Ituring';
+console.log(variavel1);
+console.log(variavel2);
+
+variavel1 = 'Discord';
+console.log(variavel1);
+console.log(variavel2);
+
+// operadores matematicos
+
+let numero1 = 100;
+let numero2 = 300;
+
+console.log(numero1 + numero2);
+
+let gorjeta1 = 1;
+let gorjeta2 = 2;
+
+console.log('Valor final da gorjeta do garçom');
+console.log(gorjeta1 + gorjeta2);
+
+//somando textos
+let texto1 = 'ITuring';
+let texto2 = 'Aprenda a programar';
+let resultado = texto1 + texto2;
+
+console.log(texto1);
+console.log(texto2);
+console.log('Ituring - Aprenda a programar');
+
+//calculo gorjeta
+let contaDoBar = 1000;
+let parcelaGorjeta = 0.1;
+let gorjetaDoGarcom = contaDoBar * parcelaGorjeta;
+
+console.log('A gorjeta do garçom é igual a');
+console.log(gorjetaDoGarcom + ' reais');
